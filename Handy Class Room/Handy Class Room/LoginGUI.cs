89ccdea07a8de38c22a69dfc80c08cae5a81cd04
AlauddinMunshi <AlauddinMunshi@users.noreply.github.com>
2019-08-18 21:16:16 +0600

    new
    new

--- Handy Class Room/Handy Class Room/LoginGUI.cs
+++ Handy Class Room/Handy Class Room/LoginGUI.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Drawing;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,21 +10,11 @@ using System.Windows.Forms;
 
 namespace Handy_Class_Room
 {
-    public partial class Form1 : Form
+    public partial class LoginGUI : UserControl
     {
-        public Form1()
+        public LoginGUI()
         {
             InitializeComponent();
-        }
-
-        private void panel2_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            Application.Exit();
         }
     }
 }
